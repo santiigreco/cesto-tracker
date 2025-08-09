2e874bfcc9d41be4b6d9093c5ed6e2d23a8ca11f
santiigreco <santigreco@hotmail.com>
2025-08-08 23:12:05 -0300

    second commit

--- IAO_project/detectar_consola.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-print("Mueve el mouse a la esquina superior izquierda de la consola y presiona 'Ctrl+C' para detener.")
-while True:
-    x, y = pyautogui.position()  # Obtiene la posición actual del mouse
-    print(f"Posición del mouse: ({x}, {y})")
-    time.sleep(3)  # Pausa por medio segundo antes de actualizar
