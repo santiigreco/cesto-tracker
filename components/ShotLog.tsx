b9346bf2c381a81f3afa1e3489f6db6bf7e7d69b
santiigreco <santigreco@hotmail.com>
2025-08-14 21:34:04 -0300

    logos

--- components/ShotLog.tsx
+++ components/ShotLog.tsx
@@ -1,18 +1,13 @@
 
 import React, { useState, useMemo } from 'react';
 import { Shot, PlayerStats } from '../types';
+import DownloadIcon from './DownloadIcon';
 
 interface ShotLogProps {
   shots: Shot[];
   stats: PlayerStats[];
   playerNames: Record<string, string>;
 }
-
-const DownloadIcon: React.FC<{className?: string}> = ({className}) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className || "h-5 w-5"} viewBox="0 0 20 20" fill="currentColor">
-        <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 9.293a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-    </svg>
-);
 
 // Helper component for a progress bar
 const PercentageBar: React.FC<{ percentage: number }> = ({ percentage }) => (
--- components/StatisticsView.tsx
+++ components/StatisticsView.tsx
@@ -1,29 +1,11 @@
+
 import React, { useRef } from 'react';
 import { PlayerStats } from '../types';
+import DownloadIcon from './DownloadIcon';
+import TrophyIcon from './TrophyIcon';
 
 // TypeScript declaration for html2canvas global variable
 declare const html2canvas: any;
-
-const DownloadIcon: React.FC<{className?: string}> = ({className}) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className || "h-5 w-5"} viewBox="0 0 20 20" fill="currentColor">
-        <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 9.293a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-    </svg>
-);
-
-const TrophyIcon: React.FC<{ rank: number }> = ({ rank }) => {
-  const colors: { [key: number]: string } = {
-    1: 'text-yellow-400', // Gold
-    2: 'text-gray-300',   // Silver
-    3: 'text-yellow-600', // Bronze
-  };
-  const color = colors[rank] || 'text-gray-500';
-
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" className={`h-8 w-8 ${color}`} viewBox="0 0 20 20" fill="currentColor">
-      <path d="M10 2a6 6 0 00-6 6c0 1.887.646 3.633 1.726 5H4a1 1 0 00-1 1v2a1 1 0 001 1h1.583A6.012 6.012 0 0010 18a6.012 6.012 0 004.417-2H16a1 1 0 001-1v-2a1 1 0 00-1-1h-1.726A5.969 5.969 0 0016 8a6 6 0 00-6-6zm-3.293 9.293a1 1 0 010 1.414L5 14.414A3.99 3.99 0 0110 16a3.99 3.99 0 015-1.707l-1.707-1.707a1 1 0 010-1.414L15 10.293A4.004 4.004 0 0114 8c0-1.312-.636-2.5-1.682-3.268L10 7.05l-2.318-2.318C6.636 5.5 6 6.688 6 8c0 .79.23 1.523.634 2.121L3.293 11.293z" />
-    </svg>
-  );
-};
 
 const DonutChart: React.FC<{ percentage: number; size?: number; strokeWidth?: number; }> = ({ percentage, size = 120, strokeWidth = 15 }) => {
   const radius = (size - strokeWidth) / 2;
